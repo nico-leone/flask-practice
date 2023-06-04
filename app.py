bb0e69f23368964b67c0e03eb87fcee51a411ec9
nico-leone <54903060+nico-leone@users.noreply.github.com>
2023-06-04 21:46:04 +0000

    this commit adds dynamic data in the form of jobs, as well as an api route for json information

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+{
+  'id' : 1,
+  'title' : 'Imposter',
+  'location' : 'Medbay',
+  'salary' : '$ 35'
+}, 
+  {
+  'id' : 2,
+  'title' : 'Crewmate',
+  'location' : 'Engine',
+}, 
+  {
+  'id' : 3,
+  'title' : 'Imposter',
+  'location' : 'Comms',
+  'salary' : '$ 35'
+}
+
+  
+]
+
 #Gives a route to the app, just a / indicates the default home page.
 @app.route("/")
-def hello_world():
-  return render_template('home.html')#imported seperatley at the top, render template is how we will display html files.
+def hello_world():                    #here we pass the information stored in JOBS through the arg
+  return render_template('home.html', jobs=JOBS)#imported seperatley at the top, render template is how we will display html files.
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS);
 
 #simpler way to have the file run
 if __name__ == "__main__":
