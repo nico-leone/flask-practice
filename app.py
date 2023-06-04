a2ab446ea143cb6e0581126aae82259bb9ad17a0
nico-leone <54903060+nico-leone@users.noreply.github.com>
2023-06-04 20:58:54 +0000

    the sussy commit, added html, css, and bootstrap usage

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Gives a route to the app, just a / indicates the default home page.
 @app.route("/")
 def hello_world():
-  return "Hello, world"
+  return render_template('home.html')#imported seperatley at the top, render template is how we will display html files.
 
-
+#simpler way to have the file run
 if __name__ == "__main__":
   app.run(host ='0.0.0.0', debug=True)
 
