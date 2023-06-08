57864c333018f7da99425735c153fd0a0afff4fa
nico-leone <54903060+nico-leone@users.noreply.github.com>
2023-06-08 01:04:39 +0000

    database connection, the page is no longer static, it can now be changed through the database.

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-{
-  'id' : 1,
-  'title' : 'Imposter',
-  'location' : 'Medbay',
-  'salary' : '$ 35'
-}, 
-  {
-  'id' : 2,
-  'title' : 'Crewmate',
-  'location' : 'Engine',
-}, 
-  {
-  'id' : 3,
-  'title' : 'Imposter',
-  'location' : 'Comms',
-  'salary' : '$ 35'
-}
-
-  
-]
-
 #Gives a route to the app, just a / indicates the default home page.
 @app.route("/")
-def hello_world():                    #here we pass the information stored in JOBS through the arg
-  return render_template('home.html', jobs=JOBS)#imported seperatley at the top, render template is how we will display html files.
+def hello_world():#here we pass the information stored in JOBS through the arg
+  
+  jobs = load_jobs_from_db()#rather than the hardcoded list, we are now pulling the jobs from the database.
+  return render_template('home.html', jobs=jobs, company_name='among us')#imported seperatley at the top, render template is how we will display html files.
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS);
+  jobs = dict(load_jobs_from_db())
+  return jsonify(jobs);
 
 #simpler way to have the file run
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,10 +1,12 @@
+#this file pretty much just acts as a connector to the database.
 from sqlalchemy import create_engine, text
+import os
 
 #the parameters for this are a template to connect to a database through msql/mariadb that can be found
 #here: https://docs.sqlalchemy.org/en/20/dialects/mysql.html, along with the rest of the information on
 #how to set up a database using sqlalchemy. You can see from the mysql+pymysql that we need to 
 #install the pymysql library to use as a connector.
-db_connection_string = "mysql+pymysql://4l58fwwryik8xfzu5exu:pscale_pw_t7Dua0RMOBQz9u47UNb8RVXzHaDcvg1AnYje2lxOdA9@aws.connect.psdb.cloud/flask_test?charset=utf8mb4"
+db_connection_string = os.environ['DC_CONNECTION_STRING']
 
 #information found in the ssl secion of https://docs.sqlalchemy.org/en/20/dialects/mysql.html#mysqldb-ssl,
 #you need to replace the ca section with the one found in planetscale.coms args.
@@ -15,9 +17,17 @@ engine = create_engine(db_connection_string,
                         }
                       })
 
-with engine.connect() as conn:
-  result = conn.execute(text('select * from jobs'))
-  print("type(result):", type(result))
-  result_all = result.all()
-  print("type(result.all())", type(result_all))
-  print("result", result_all)
+
+#function to load the jobs from the database, comments throughout explain.
+def load_jobs_from_db():
+  #here we connect with the engine
+  with engine.connect() as conn:
+  #this function makes an sql call and stores the return in the "result"
+    result = conn.execute(text('select * from jobs'))
+
+  #iterate through the rows returned by the above statement, converting them to dicts and storing them in the array.
+    jobs = []#list of dictionaries.
+    for row in result.all():
+      #here we convert the sql row into a python dict, a type of data structure that stores value name and value.
+      jobs.append(row._mapping)
+    return jobs#returns the list of dictionaries.
